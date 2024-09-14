26ec3925fcdcb669052220b536cee11dd6edb1e3
redaphid <iam@aaronherres.com>
2024-09-14 03:50:29 -0700

    add date to default

--- batchers/linear.mjs
+++ batchers/linear.mjs
@@ -1,5 +1,6 @@
 export default async function linear({frame,max}) {
   const percent = (frame / max);
   console.log(percent);
-  return { "Sampler.noise_seed": Math.floor(percent * 1000), "save.filename_prefix": `THE_SINK/batching-cli/1/1` };
+  const isoDate = new Date().toISOString().split('T')[0];
+  return { "Sampler.noise_seed": Math.floor(percent * 1000), "save.filename_prefix": `THE_SINK/batching-cli/${isoDate}/1` };
 }
